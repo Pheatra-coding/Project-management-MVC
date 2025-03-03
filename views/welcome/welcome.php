ecf18758c3edda00b41def1c0f84ac12a19cf508
Pheatra-coding <oempheaktra877@gmail.com>
2025-03-03 08:02:30 +0700

    project structure

---
+++ views/welcome/welcome.php
@@ -0,0 +1,16 @@
+<!-- <?php 
+// if (session_status() == PHP_SESSION_NONE) {
+//     session_start();
+// }
+// if (isset($_SESSION['user_id'])) :
+ ?> -->
+
+<!-- <?php 
+// else: 
+//     $this->redirect("/users/login"); 
+// endif;   
+?> -->
+
+<main id="main" class="main">
+    <h1>Welcome to PHP</h1>
+</main><!-- End #main -->
